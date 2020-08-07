f69ac04a8d9175bfdf608e080da0b3fb2a2faeca
ChiaLing <tobeciolynn@gmail.com>
2020-08-07 11:17:54 +0800

    add metiral module

--- lynn-org/apps/ui-lab/src/app/app.module.ts
+++ lynn-org/apps/ui-lab/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
